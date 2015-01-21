9e2c1a4b362aaa0ce4932a22a16d85f6de0783f9
Bjoern Petri <bjoern.petri@sundevil.de>
2015-01-21 18:07:26 +0100

    some more clean up

--- discover/src/main/java/net/freifunk/android/discover/Main.java
+++ discover/src/main/java/net/freifunk/android/discover/Main.java
@@ -53,6 +53,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 
 import net.freifunk.android.discover.model.NodeMap;
@@ -73,8 +74,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class Main extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks, GmapsFragment.Callbacks {
+public class Main extends ActionBarActivity implements GmapsFragment.Callbacks {
 
     private Menu optionsMenu;
 
@@ -82,10 +82,6 @@ public class Main extends ActionBarActivity
     private static final String TAG = "Main";
 
     private static HashMap<String, NodeMap> mapList = null;
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
@@ -118,15 +114,8 @@ public class Main extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setDefaultUncaughtExceptionHandler();
         setContentView(R.layout.activity_main);
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+
         mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-
 
         DatabaseHelper databaseHelper = DatabaseHelper.getInstance(this.getApplicationContext());
 
@@ -134,7 +123,9 @@ public class Main extends ActionBarActivity
         LoadNodeMapDatabaseTask loadNodeMapsDatabaseTask = new LoadNodeMapDatabaseTask();
         loadNodeMapsDatabaseTask.execute(new NodeMap[]{});
 
-    }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, GmapsFragment.newInstance(GmapsFragment.NODES_TYPE)).commit();
+        }
 
 
     private LatLng getLocation() {
@@ -167,37 +158,6 @@ public class Main extends ActionBarActivity
     }
 
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        switch (position) {
-            case 0:
-                fragmentManager.beginTransaction().replace(R.id.container, GmapsFragment.newInstance(GmapsFragment.NODES_TYPE)).commit();
-                break;
-            default:
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                        .commit();
-                break;
-        }
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-/*           case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-                */
-        }
-    }
-
     public void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -205,28 +165,20 @@ public class Main extends ActionBarActivity
         actionBar.setTitle(mTitle);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         this.optionsMenu = menu;
+        getMenuInflater().inflate(R.menu.main, menu);
+        restoreActionBar();
 
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-
-            /* check whether requests are already running */
-            if (RequestQueueHelper.getInstance().size() > 0) {
-                setRefreshActionButtonState(true);
-            }
-
-            return true;
+        /* check whether requests are already running */
+        if (RequestQueueHelper.getInstance().size() > 0) {
+            setRefreshActionButtonState(true);
         }
-        return super.onCreateOptionsMenu(menu);
-    }
+
+        return true;
+   }
 
     public void setRefreshActionButtonState(final boolean refreshing) {
 
@@ -281,50 +233,6 @@ public class Main extends ActionBarActivity
 
     @Override
     public void onMarkerClicked(Object o) {
-
-    }
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((Main) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
 
     void updateMaps() {
